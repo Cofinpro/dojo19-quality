8832998537cb262b3372a03992ebb249456cca67
Christian Huff <christian.huff@cofinpro.de>
2019-04-26 14:58:25 +0200

    Moved main loop into separate class. Added initial unit tests for the main loop and the game.

--- Philipp_Christian/trivia-java/src/main/java/com/adaptionsoft/games/trivia/runner/GameRunner.java
+++ Philipp_Christian/trivia-java/src/main/java/com/adaptionsoft/games/trivia/runner/GameRunner.java
@@ -3,34 +3,18 @@ package com.adaptionsoft.games.trivia.runner;
 import java.util.Random;
 
 import com.adaptionsoft.games.uglytrivia.Game;
+import com.adaptionsoft.games.uglytrivia.MainLoop;
 
 
 public class GameRunner {
 
-	private static boolean notAWinner;
-
 	public static void main(String[] args) {
-		Game aGame = new Game();
-		
-		aGame.add("Chet");
-		aGame.add("Pat");
-		aGame.add("Sue");
-		
-		Random rand = new Random();
-	
-		do {
-			
-			aGame.roll(rand.nextInt(5) + 1);
-			
-			if (rand.nextInt(9) == 7) {
-				notAWinner = aGame.wrongAnswer();
-			} else {
-				notAWinner = aGame.wasCorrectlyAnswered();
-			}
-			
-			
-			
-		} while (notAWinner);
-		
+		Game game = new Game();
+		Random random = new Random();
+		MainLoop mainLoop = new MainLoop(game, random);
+
+		mainLoop.runGame();
 	}
+
+
 }
